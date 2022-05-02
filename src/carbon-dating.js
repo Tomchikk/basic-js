a5c6e1a77b36b9a3597491c1fc67f41dac1bf68f
Tomchikk <tamara.isakova7171@gmail.com>
2022-05-02 23:46:10 +0300

    carbon ok

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample( sampleActivity ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (( typeof sampleActivity !== "string" ) || ( isNaN(sampleActivity )) )
+    return false  
+  else {
+    const years = - Math.log( sampleActivity / MODERN_ACTIVITY ) * HALF_LIFE_PERIOD / Math.LN2
+    return (( Math.ceil(years) !== Infinity ) && ( Math.ceil(years) > 0 )) ? Math.ceil(years) : false
+  }
 }
 
 module.exports = {
